fe2254e98b4dda3a001bd422027f8e2e7fb7055e
AWS-First-Cloud-Journey <awsfcj@gmail.com>
2023-08-28 20:44:39 +0700

    update

--- bin/ecs-chatbot-app.ts
+++ bin/ecs-chatbot-app.ts
@@ -1,21 +1,36 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { EcsChatbotAppStack } from '../lib/ecs-chatbot-app-stack';
+import * as cdk from "aws-cdk-lib";
+import { EcsStack } from "../lib/ecs-stack";
+import { CodePipelineStack } from "../lib/codepipeline-stack";
+import { EcrStack } from "../lib/ecr-stack";
 
 const app = new cdk.App();
-new EcsChatbotAppStack(app, 'EcsChatbotAppStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// create a ecr repository
+const ecr = new EcrStack(app, "EcrStack", {
+  repoName: "aws-fcj-repo",
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// create an ecs cluster
+const ecs = new EcsStack(app, "Ecs-CDK-Stack", {
+  vpcId: "vpc-0861e4475acf1ac1d",
+  vpcName: "project-vpc",
+  ecrRepoName: ecr.repoName,
+  env: {
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
+});
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// create a pipeline
+new CodePipelineStack(app, "CodePipelineChatbotStack", {
+  repoName: "aws-fcj-repo",
+  repoBranch: "main",
+  repoOwner: "awscommunitybuilder",
+  ecrRepoName: ecr.repoName,
+  service: ecs.service,
+  env: {
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
 });
